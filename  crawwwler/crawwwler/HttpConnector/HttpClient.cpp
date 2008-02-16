1b5e5597e30852ecdcbbd0705aacd9918677a4a9
davidjohnarcher <davidjohnarcher@a9951ad4-3145-0410-9025-0dd7c1825583>
2008-02-16 18:05:18 +0000

    Fleshing out of the GetRequest() method

---  crawwwler/crawwwler/HttpConnector/HttpClient.cpp
+++  crawwwler/crawwwler/HttpConnector/HttpClient.cpp
@@ -3,6 +3,9 @@
 #include "../HttpUtils/HttpResponse.h"
 #include "../HttpUtils/Url.h"
 #include "../HttpUtils/Port.h"
+#include "../HttpUtils/Socket.h"
+#include "../HttpUtils/HttpRequest.h"
+#include "../GlobalData/GlobalData.h"
 
 namespace Crawwwler {
 
@@ -19,7 +22,59 @@ CHttpClient::~CHttpClient() {
 // Public Methods
 
 bool CHttpClient::GetResource(const CUrl& Url, const CCPort& Port, CHttpResponse *pResponse) const {
-	return NULL;
+	// Check args
+	if (!pResponse) return false;
+
+	// Build the request
+	CHttpRequest Request;
+	if (!SetRequest(Url.GetServer(), Url.GetResource(), &Request))
+
+	// Send it off
+	// This stupid thing won't compile if I delete CSocket r. Try it!!!
+	CSocket r;
+	CSocket Socket;
+
+	return Socket.SendRequest(Url.GetServer(), Port.GetPortNum(), Request, pResponse);
+}
+
+bool CHttpClient::SetRequest(const std::string& ServerName, const std::string& Resource, CHttpRequest *pRequest) const {
+	// Check parameters
+	if (!pRequest) return false;
+
+	// This is an Http GET request
+	pRequest->SetType(REQUEST_GET);
+
+	// Headers
+	{
+		// The user agent
+		std::string UserAgent(GlobalData::CRAWLER_NAME);
+		tHeaderType HeaderType = HEADER_USER_AGENT;
+		CHttpHeader UserAgentHeader(HeaderType, UserAgent);
+		if (!pRequest->AddHeader(UserAgentHeader)) return false;
+
+		// The host
+		HeaderType = HEADER_HOST;
+		CHttpHeader HostHeader(HeaderType, ServerName);
+		if (!pRequest->AddHeader(HostHeader)) return false;
+
+		HeaderType = HEADER_PROXY_CONNECTION;
+		CHttpHeader ProxyHeader(HeaderType, "Keep-Alive");
+		if (!pRequest->AddHeader(ProxyHeader)) return false;
+
+		HeaderType = HEADER_PRAGMA;
+		CHttpHeader PragmaHeader(HeaderType, "no-cache");
+		if (!pRequest->AddHeader(PragmaHeader)) return false;
+
+		HeaderType = HEADER_CONNECTION;
+		CHttpHeader ConnectionHeader(HeaderType, "close");
+		if (!pRequest->AddHeader(ConnectionHeader)) return false;
+	}
+
+	// Set the target resource
+	pRequest->SetResource(Resource);
+
+	// Success
+	return true;
 }
 
 ///////////////////////////////////////////////////////////////
---  crawwwler/crawwwler/HttpConnector/HttpClient.h
+++  crawwwler/crawwwler/HttpConnector/HttpClient.h
@@ -4,6 +4,8 @@
 #ifndef HTTPCLIENT_H_
 #define HTTPCLIENT_H_
 
+#include <iostream>
+
 namespace Crawwwler {
 
 class CHttpClient {
@@ -13,6 +15,9 @@ public:
 
 	// Try to get the resource, returns false on web failure, not incorrect resource. Raw response is returned via pResponse
 	bool GetResource(const class CUrl& Url, const class CCPort& Port, class CHttpResponse *pResponse) const;
+
+private:
+	bool SetRequest(const std::string& ServerName, const std::string& Resource, class CHttpRequest *pRequest) const;
 };
 
 }
