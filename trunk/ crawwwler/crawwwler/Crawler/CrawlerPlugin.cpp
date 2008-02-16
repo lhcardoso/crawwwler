9bf188638911c2148c1a981958d22fbcf26ace4e
davidjohnarcher <davidjohnarcher@a9951ad4-3145-0410-9025-0dd7c1825583>
2008-02-16 18:04:27 +0000

    Starting deprecation of CCrawlerPlugin

--- trunk/ crawwwler/crawwwler/Crawler/CrawlerPlugin.cpp
+++ trunk/ crawwwler/crawwwler/Crawler/CrawlerPlugin.cpp
@@ -13,7 +13,7 @@ namespace Crawwwler
 ///////////////////////////////////////////////////////////////
 // Constructors
 
-CCrawlerPlugin::CCrawlerPlugin() : m_Connector() {
+CCrawlerPlugin::CCrawlerPlugin() {
 }
 
 CCrawlerPlugin::~CCrawlerPlugin() {
@@ -24,7 +24,7 @@ CCrawlerPlugin::~CCrawlerPlugin() {
 		delete pCurrent;
 		pCurrent = NULL;
 	}
-	
+
 }
 
 ///////////////////////////////////////////////////////////////
@@ -37,7 +37,7 @@ bool CCrawlerPlugin::Init() {
 
 bool CCrawlerPlugin::Start() {
 	// Pass on the call
-	return StartCrawl(); 
+	return StartCrawl();
 }
 
 bool CCrawlerPlugin::Stop() {
@@ -54,14 +54,14 @@ bool CCrawlerPlugin::AddServer(CServerInfo *pServer) {
 
 std::list<CHttpResponse*> CCrawlerPlugin::CollectResponses() {
 	// ### This method will eventually need to be thread safe, but, erm, how? In, fact this whole class will probably have to be thread safe.
-	
+
 	std::list<CHttpResponse*> Result;
-	
+
 	// Make a copy of the all reposnses for each of the servers
 	for (std::list<CServerInfo*>::iterator i = m_Servers.begin(); i != m_Servers.end(); i++) {
 		CServerInfo *pCurrent = *i;
 		std::list<CHttpResponse*> *pResponses = pCurrent->GetResponses();
-		
+
 		// Copy all the responses for this server
 		for (std::list<CHttpResponse*>::iterator i = pResponses->begin(); i != pResponses->end(); i++) {
 			CHttpResponse *pCurrentResponse = *i;
@@ -69,22 +69,22 @@ std::list<CHttpResponse*> CCrawlerPlugin::CollectResponses() {
 		}
 		// Don't delete pCurrent because it is now owned by the caller
 	}
-	
+
 	// Clear the list
 	m_Servers.clear();
-	
+
 	return Result;
 }
 
 bool CCrawlerPlugin::PushData(std::list<void*> Data) {
-	
+
 	// Copy the data since, the caller still owns it
 	for (std::list<void*>::iterator i = Data.begin(); i != Data.end(); i++) {
 		// ### Use of the copy constructor!!!
 		CServerInfo *pCurrent = (CServerInfo*)*i;
 		m_Servers.push_back(pCurrent);
 	}
-	
+
 	return true;
 }
 
@@ -95,14 +95,11 @@ bool CCrawlerPlugin::StartCrawl() {
 	// If there are no servers to crawl, then there is nothing to do
 	// ### This is wrong since we will be able to add servers from time to time and this class should just wait
 	if (m_Servers.size() == 0) return false;
-	
+
 	// Crawl through all required resources on each server
 	for (std::list<CServerInfo*>::iterator i = m_Servers.begin(); i != m_Servers.end(); i++) {
 		CServerInfo *pCurrent = *i;
-		
-		// The CServerInfo class maintains ownership
-		CRemoteServer *pServer = pCurrent->GetServer();
-		
+
 		// Crawl all resources on this server
 		std::string Resource;
 		while (pCurrent->PopResource(&Resource)) {
@@ -110,104 +107,64 @@ bool CCrawlerPlugin::StartCrawl() {
 			CHttpRequest Request;
 			{
 				// Set the request
-				if (!SetRequest(Resource, &Request, pServer)) {
+				if (false) {
 					// Couldn't set the request so ignore this server
 					continue;
 				}
 			}
-			
+
 			// Send the request to this server. Create the object on the heap so that the serverinfo can take ownership
 			CHttpResponse *pResponse = new CHttpResponse();
-			
-			
-			if (!m_Connector.SendRequest(*pServer, Request, &pResponse)) {
+
+
+			if (false) {
 				// ### Might have to register a failure reason and the fact that the request wasn't successful
 				delete pResponse;
 				pResponse = NULL;
 				continue;
 			}
-			
+
 			// Associate the response received with the resource requested
 			if (!pResponse->SetResource(Resource)) return false;
-			
+
 			// Store the response
 			if (!pCurrent->AddResponse(pResponse)) return false;
 			// pCurrent now has ownership
 			pResponse = NULL;
 		}
-		
+
 		// Don't delete pCurrent since we still need it
 		pCurrent = NULL;
 	}
-	
+
 	// Report back on findings
 	if (!Announce()) return false;
 	// Clear the list of servers, by now they are somebody elses responsibility
 	m_Servers.clear();
-	
+
 	// Successful crawl!
 	return true;
 }
 
-bool CCrawlerPlugin::SetRequest(std::string Resource, CHttpRequest *pRequest, CRemoteServer *pServer) {
-	// Check parameters
-	if (!pRequest) return false;
-	
-	// This is an Http GET request
-	pRequest->SetType(REQUEST_GET);
-	
-	// Headers
-	{
-		// The user agent
-		std::string UserAgent(GlobalData::CRAWLER_NAME);
-		tHeaderType HeaderType = HEADER_USER_AGENT;
-		CHttpHeader UserAgentHeader(HeaderType, UserAgent);
-		if (!pRequest->AddHeader(UserAgentHeader)) return false;
-		
-		// The host
-		HeaderType = HEADER_HOST;
-		CHttpHeader HostHeader(HeaderType, pServer->HostName());
-		if (!pRequest->AddHeader(HostHeader)) return false;
-		
-		HeaderType = HEADER_PROXY_CONNECTION;
-		CHttpHeader ProxyHeader(HeaderType, "Keep-Alive");
-		if (!pRequest->AddHeader(ProxyHeader)) return false;
-		
-		HeaderType = HEADER_PRAGMA;
-		CHttpHeader PragmaHeader(HeaderType, "no-cache");
-		if (!pRequest->AddHeader(PragmaHeader)) return false;
-		
-		HeaderType = HEADER_CONNECTION;
-		CHttpHeader ConnectionHeader(HeaderType, "close");
-		if (!pRequest->AddHeader(ConnectionHeader)) return false;
-	}
-	
-	// Set the target resource
-	pRequest->SetResource(Resource);
-	
-	// Success
-	return true;
-}
-
 bool CCrawlerPlugin::Announce() {
-	
+
 	// Nothing to do if there is no data, which is fine
 	if (m_Servers.size() == 0) return true;
-	
+
 	// Convert the CServerInfo list to a void list
 	std::list<void*> Data;
 	for (std::list<CServerInfo*>::iterator i = m_Servers.begin(); i != m_Servers.end(); i++) {
 		// Is this a copy or a new pointer to the original??
 		Data.push_back(*i);
 	}
-	
+
 	// Let each of the subscribers know about our findings
 	for (std::list<CPlugin*>::iterator i = m_Subscribers.begin(); i != m_Subscribers.end(); i++) {
 		CPlugin* pCurrent = *i;
 		// ### This should be a pointer to a list else it will be way too slow
 		if (!pCurrent->PushData(Data)) return false;
 	}
-	
+
 	return true;
 }
 
--- trunk/ crawwwler/crawwwler/Crawler/CrawlerPlugin.h
+++ trunk/ crawwwler/crawwwler/Crawler/CrawlerPlugin.h
@@ -4,7 +4,6 @@
 #define CRAWLERPLUGIN_H_
 
 #include "../PluginManager/Plugin.h"
-#include "../HttpUtils/HttpConnector.h"
 #include "../ServerInfo/ServerInfo.h"
 
 namespace Crawwwler
@@ -17,36 +16,34 @@ class CCrawlerPlugin : public CPlugin {
 public:
 	CCrawlerPlugin();
 	virtual ~CCrawlerPlugin();
-	
+
 	//Overridden
 	bool Init();
 	//Overridden
 	bool Start();
 	//Overridden
 	bool Stop();
-	
+
 	// Add a server to the list of sites that this crawler must crawl, this class takes ownership of the memory
 	bool AddServer(CServerInfo *pServer);
 	// ### Possibly use this method
 	//bool AddKeyword();
-	
+
 	// Collect all the responses from this crawl so far, the caller now owns the list and items as it is cleared from this object
 	std::list<CHttpResponse*> CollectResponses();
 	// Get the number of servers
 	int GetNumServers() { return m_Servers.size(); };
-	
+
 	// Overridden
 	virtual bool PushData(std::list<void*> Data);
 private:
 	// The http connector that this crawler will use to connect to the web
-	CHttpConnector m_Connector;
+	//CHttpConnector m_Connector;
 	// The list of servers this crawler must crawl.  This class is responsible for the memory
 	std::list<CServerInfo*> m_Servers;
-	
+
 	bool StartCrawl();
-	// Set the next request with the resource
-	bool SetRequest(std::string Resource, CHttpRequest *pRequest, CRemoteServer *pServer);
-	
+
 	// Report back on findings
 	bool Announce();
 };
