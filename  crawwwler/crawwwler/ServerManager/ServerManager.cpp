386a34ba14a713e030f0487baa6a6ac15915ba39
davidjohnarcher <davidjohnarcher@a9951ad4-3145-0410-9025-0dd7c1825583>
2008-02-16 00:24:43 +0000

    Name change to CServerCrawler

---  crawwwler/crawwwler/ServerManager/ServerManager.cpp
+++  crawwwler/crawwwler/ServerManager/ServerManager.cpp
@@ -3,12 +3,55 @@
 namespace Crawwwler
 {
 
-CServerManager::CServerManager()
-{
+///////////////////////////////////////////////////////////////
+// Constructors
+
+CServerCrawler::CServerCrawler() {
 }
 
-CServerManager::~CServerManager()
-{
+CServerCrawler::~CServerCrawler() {
 }
+
+///////////////////////////////////////////////////////////////
+// Public Methods
+
+bool CServerCrawler::Crawl() {
+	// Get the robots.txt file
+	CHttpResponse RawRobotsFile = m_HttpClient.Crawl("robots.txt");
+
+	// Process the robots file
+	CRobotsTxtFile RobotsFile(RawRobotsFile);
+
+	// Start crawling from the index page
+	CManagedUrlList Urls;
+	Urls.Add("/");
+
+	// Recursively crawl the site
+	while (!Urls.Empty()) {
+		// Crawl the current target
+		CUrl Current = GetNext();
+
+		// Make sure the robots file allows this
+		if (!RobotsFile.Allows(Current)) {
+			continue;
+		}
+
+		// Crawl the current target
+		CHttpResponse Response = m_HttpClient.Crawl(Current);
+		// Extract the urls from this response
+		std::list<CUrl> ExtractedUrls = m_UrlExtractor.ExtractFrom(Response);
+
+		// Add uniquely to the managed list of urls
+		Urls.Add(ExtractedUrls);
+	}
+
+	// Successful crawl
+}
+
+
+///////////////////////////////////////////////////////////////
+// Private Methods
+
+
 
 }
---  crawwwler/crawwwler/ServerManager/ServerManager.h
+++  crawwwler/crawwwler/ServerManager/ServerManager.h
@@ -1,23 +1,30 @@
-// ServerManager.h
+// ServerCrawler.h
 // Manages the crawling of a remote server
 
 #ifndef SERVERMANAGER_H_
 #define SERVERMANAGER_H_
 
+#include "../HttpUtils/RemoteServer.h"
+
 namespace Crawwwler
 {
 
-class CServerManager
-{
+class CServerCrawler {
 public:
-	CServerManager();
-	virtual ~CServerManager();
+	CServerCrawler();
+	virtual ~CServerCrawler();
 
-
+	bool Crawl();
 
 private:
-	CServerManager(const CServerManager& Other);
-	CServerManer& operator=(const CServerManager& Rhs);
+	CServerCrawler(const CServerCrawler& Other);
+	CServerCrawler& operator=(const CServerCrawler& Rhs);
+
+	// Server name to crawl
+	std::string Servername;
+
+
+
 };
 
 }
