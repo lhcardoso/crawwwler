b0fd9f4245f0e95daa5787219fdc9b1a8254517f
davidjohnarcher <davidjohnarcher@a9951ad4-3145-0410-9025-0dd7c1825583>
2008-02-16 19:57:30 +0000

    Thinning out UrlExtractor to eventually no longer be of type CPlugin Added new ExtractFrom() method which will deprecate a lot of the current functionality

---  crawwwler/crawwwler/UrlExtractor/UrlExtractor.cpp
+++  crawwwler/crawwwler/UrlExtractor/UrlExtractor.cpp
@@ -1,8 +1,8 @@
 #include "UrlExtractor.h"
 #include "../HttpUtils/HttpResponse.h"
 #include "../HttpUtils/Url.h"
-#include <fstream.h>
-#include <iostream>
+#include "../Url/ManagedUrlList.h"
+
 
 namespace Crawwwler {
 
@@ -21,10 +21,10 @@ CUrlExtractor::~CUrlExtractor()
 // Public Methods
 
 bool CUrlExtractor::Start() {
-	
+
 	// Nothing to do if we have no data, which is fine
 	if (m_ServerData.size() == 0) return true;
-	// Process the data for each server in the list 
+	// Process the data for each server in the list
 	for (std::list<CServerInfo*>::iterator i = m_ServerData.begin(); i != m_ServerData.end(); i++) {
 		CServerInfo *pServerInfo = *i;
 		if (!ProcessServer(pServerInfo)) {
@@ -37,28 +37,51 @@ bool CUrlExtractor::Start() {
 			pServerInfo->NewResources();
 		}
 	}
-	
+
 	if (!Announce()) return false;
-	
-	
-	
+
+
+
 	// We're done with our data
 	m_ServerData.clear();
-	
+
 	// Success :-)
 	return true;
-	
+
+}
+
+bool CUrlExtractor::ExtractFrom(CHttpResponse& File, CManagedUrlList* pList) {
+	// Parse the raw response
+	if (!File.Parse()) {
+		// Ignore this one??
+		// ### Can't really do this, though it could mean there is no raw response!!!
+		return false;
+	}
+
+	// Add all unique urls (not in-page duplicates) exposed by this response
+	std::list<CUrl> Urls = File.GetUrls();
+	for (std::list<CUrl>::iterator i = Urls.begin(); i != Urls.end(); i++) {
+		pList->AddUnique(*i);
+	}
+	// Go through the list of discovered resources. Add the local ones to the list of pages to retrieve
+	// and the remote ones, well, the remote ones
+	//std::string ServerName = pServerInfo->GetServerName();
+	//std::list<CUrl> LocalUrls = pCurrent->GetLocalUrls(ServerName);
+	//if (!pServerInfo->EmptyResources()) return false;
+	//if (!pServerInfo->AddNewUrls(LocalUrls)) return false;
+
+	return true;
 }
 
 bool CUrlExtractor::PushData(std::list<void*> Data) {
-	
+
 	// Copy the data since, the caller still owns it
 	for (std::list<void*>::iterator i = Data.begin(); i != Data.end(); i++) {
 		// ### Use of the copy constructor!!!
 		CServerInfo *pCurrent = (CServerInfo*)*i;
 		m_ServerData.push_back(pCurrent);
 	}
-	
+
 	return true;
 }
 
@@ -66,24 +89,21 @@ bool CUrlExtractor::PushData(std::list<void*> Data) {
 // Private Methods
 
 bool CUrlExtractor::ProcessServer(CServerInfo *pServerInfo) {
-	
-	// Get hold of the responses	
+
+	// Get hold of the responses
 	std::list<CHttpResponse*> *pResponses = pServerInfo->GetResponses();
 	// Parse each response and remember that we have already visited that resource
 	for (std::list<CHttpResponse*>::iterator i = pResponses->begin(); i != pResponses->end(); i++) {
 		CHttpResponse *pCurrent = *i;
-		
+
 		// Parse the raw response
 		if (!pCurrent->Parse()) {
 			// Ignore this one??
 			// ### Can't really do this, though it could mean there is no raw response!!!
 			return false;
 		}
-		
+
 		// Remember that we have already crawled this page
-		ofstream p("/home/dave/be.txt");
-		p << pCurrent->GetRawResult()->c_str();
-		p.close();
 		const CHttpHeader *pLocationHeader = pCurrent->GetHeader(HEADER_CONTENT_LOCATION);
 		if (!pLocationHeader) {
 			// We might not have received a content location header so just use the resource we set
@@ -92,32 +112,28 @@ bool CUrlExtractor::ProcessServer(CServerInfo *pServerInfo) {
 			pLocationHeader = new CHttpHeader(HEADER_CONTENT_LOCATION, pCurrent->GetResource());
 			if (!pLocationHeader) return false;
 		}
-		bool bResult = ProcessVisitedResource(*pLocationHeader, pServerInfo); 
-		
+
 		// Delete this temporary copy of the header
 		delete pLocationHeader;
 		pLocationHeader = NULL;
-		
-		if (!bResult) return false;
 	}
-	
+
 	// Clear the list of resources to visit
 	if (!pServerInfo->ClearTargetResources()) return false;
 
 	// Process the newly discovered resources
 	if (!ProcessNewResources(pResponses, pServerInfo)) return false;
-	
+
 	// Empty the responses
 	pServerInfo->EmptyResponses();
-	
-	// TODO add to the list of parsed responses
+
 	return true;
 }
 
 bool CUrlExtractor::Announce() {
 	// Nothing to do if there is no data, which is fine
 	if (m_ServerData.size() == 0) return true;
-	
+
 	// Convert the CServerInfo list to a void list
 	std::list<void*> Data;
 	for (std::list<CServerInfo*>::iterator i = m_ServerData.begin(); i != m_ServerData.end(); i++) {
@@ -126,55 +142,23 @@ bool CUrlExtractor::Announce() {
 		if (!pCurrent->HasResources()) continue;
 		Data.push_back(*i);
 	}
-	
+
 	// Let each of the subscribers know about our findings
 	for (std::list<CPlugin*>::iterator i = m_Subscribers.begin(); i != m_Subscribers.end(); i++) {
 		CPlugin* pCurrent = *i;
 		// ### This should be a pointer to a list else it will be way too slow
 		if (!pCurrent->PushData(Data)) return false;
 	}
-	
+
 	return true;
 }
 
-bool CUrlExtractor::ProcessVisitedResource(CHttpHeader LocationHeader, CServerInfo* pServerInfo) {
-	
-	// First Get the content location directory	
-	CUrl Url;
-	if (!Url.Parse(LocationHeader.GetValue())) return false;
-	
-	// Add this page to the already visited
-	if (!pServerInfo->AddVisitedResource(Url)) return false;
-		
-	return true;
-}
 
 bool CUrlExtractor::ProcessNewResources(std::list<CHttpResponse*> *pResponses, CServerInfo *pServerInfo) {
 	// Add the discovered resources from each response to the list of resources that need to be crawled
 	for (std::list<CHttpResponse*>::iterator i = pResponses->begin(); i != pResponses->end(); i++) {
 		CHttpResponse *pCurrent = *i;
-		
-		// Save the page to disk
-		// ### Temporary!! This should be done elsewhere
-		{
-			const CHttpHeader *pLocationHeader = pCurrent->GetHeader(HEADER_CONTENT_LOCATION);
-			if (!pLocationHeader) {
-				// We might not have received a content location header so just use the resource we set
-				char ab[100];
-				sprintf(ab, pCurrent->GetResource().c_str());
-				pLocationHeader = new CHttpHeader(HEADER_CONTENT_LOCATION, pCurrent->GetResource());
-				if (!pLocationHeader) return false;
-			}
-			// The url is needed to build a filename
-			std::string Location = pLocationHeader->GetValue();
-			delete pLocationHeader;
-			pLocationHeader = NULL;
-			CUrl Url;
-			if (!Url.Parse(Location)) return false;
-			
-			if (!SaveFile(pCurrent, Url)) return false;
-		}	
-		
+
 		// Go through the list of discovered resources. Add the local ones to the list of pages to retrieve
 		// and the remote ones, well, the remote ones
 		std::string ServerName = pServerInfo->GetServerName();
@@ -182,24 +166,6 @@ bool CUrlExtractor::ProcessNewResources(std::list<CHttpResponse*> *pResponses, C
 		if (!pServerInfo->EmptyResources()) return false;
 		if (!pServerInfo->AddNewUrls(LocalUrls)) return false;
 	}
-	return true;
-}
-
-bool CUrlExtractor::SaveFile(CHttpResponse *pResponse, CUrl Url) {
-	// ### Hard coded path, change yours
-	std::string s("/home/dave/data/");
-	
-	// The filename is the name of the resource
-	std::string temp(Url.GetResource());
-	while (temp.find("/") != std::string::npos) {
-		temp = temp.substr(temp.find("/") + 1);
-	}
-	s.append(temp);
-	
-	// Save to disk
-	ofstream f(s.c_str());
-	f << pResponse->GetRawResult()->c_str();
-	f.close();
 	return true;
 }
 
