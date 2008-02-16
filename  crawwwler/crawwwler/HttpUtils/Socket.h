03d7e2610ac05319af1fa24daf50af874896e6f0
davidjohnarcher <davidjohnarcher@a9951ad4-3145-0410-9025-0dd7c1825583>
2008-02-16 18:07:26 +0000

    - Socket::SendRequest now accepts CHttpResponse* instead of ** - Adding const

---  crawwwler/crawwwler/HttpUtils/Socket.h
+++  crawwwler/crawwwler/HttpUtils/Socket.h
@@ -5,7 +5,7 @@
 #ifndef SOCKET_H_
 #define SOCKET_H_
 
-#include <string> 
+#include <string>
 
 #include <netdb.h>
 namespace Crawwwler {
@@ -20,13 +20,16 @@ class CSocket
 public:
 	CSocket();
 	virtual ~CSocket();
-	
+
 	// Send a request through the socket, returns true on success and returns a CHttpResponse object through the parameter
-	bool SendRequest(const std::string ServerName, const int PortNumber, const CHttpRequest& Request, CHttpResponse **ppResponse);
-	
+	bool SendRequest(	const std::string ServerName,
+						const int PortNumber,
+						const CHttpRequest& Request,
+						CHttpResponse *pResponse);
+
 private:
 	// Internal version called by SendRequest
-	bool Send(const std::string ServerName, const int PortNumber, CHttpRequest Request, std::string* pResult);
+	bool Send(const std::string Server, const int PortNumber, CHttpRequest Request, std::string* pResult);
 
 };
 
