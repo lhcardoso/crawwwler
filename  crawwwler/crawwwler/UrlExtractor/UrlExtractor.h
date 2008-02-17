d5b8f5d26e0ca4717b7a89ee47a259abb4057fb7
davidjohnarcher <davidjohnarcher@a9951ad4-3145-0410-9025-0dd7c1825583>
2008-02-17 17:31:58 +0000

    UrlExtractor is now simplified and isn't a plugin anymore

---  crawwwler/crawwwler/UrlExtractor/UrlExtractor.h
+++  crawwwler/crawwwler/UrlExtractor/UrlExtractor.h
@@ -9,37 +9,16 @@
 
 namespace Crawwwler {
 
-class CUrlExtractor : public CPlugin {
+class CUrlExtractor {
 public:
 	CUrlExtractor();
 	virtual ~CUrlExtractor();
-
-	// Overridden
-	virtual bool Init() { return true; };
-	// Overridden
-	virtual bool Start();
-	// Overridden
-	virtual bool Stop() { return true; };
-	// Overridden
-	virtual bool PushData(std::list<void*> Data);
-
-	//////////////////////////////////
-	// New implementation starts here
 
 	// Extract all the urls from this http response and populate pList with them
 	bool ExtractFrom(CHttpResponse& File, class CManagedUrlList* pList);
 
 private:
-	std::list<CServerInfo*> m_ServerData;
 
-	// Process the data for this server
-	bool ProcessServer(CServerInfo *pServerInfo);
-
-	// Report back on findings
-	bool Announce();
-
-	// Process all the newly discovered resources
-	bool ProcessNewResources(std::list<CHttpResponse*> *pResponses, CServerInfo *pServerInfo);
 };
 
 }
