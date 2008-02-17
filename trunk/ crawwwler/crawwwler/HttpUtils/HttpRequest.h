fa4495b5ccb83cd4565d720cec65038bcc7f3553
davidjohnarcher <davidjohnarcher@a9951ad4-3145-0410-9025-0dd7c1825583>
2008-02-17 14:30:34 +0000

    Add GetResource()

--- trunk/ crawwwler/crawwwler/HttpUtils/HttpRequest.h
+++ trunk/ crawwwler/crawwwler/HttpUtils/HttpRequest.h
@@ -14,32 +14,34 @@ namespace Crawwwler {
 		REQUEST_POST,
 		REQUEST_INVALID
 	};
-	
+
 class CHttpRequest
 {
 public:
 	CHttpRequest();
 	virtual ~CHttpRequest();
-	
+
 	// Set the type of this request
 	void SetType(tRequestType Type) { m_RequestType = Type; };
 	// Add a header to this request
 	bool AddHeader(CHttpHeader Header);
 	// Set the resource that will be requested
 	void SetResource(std::string Resource);
-	
+	// Get the resource
+	const std::string GetResource() const { return m_Resource; };
+
 	// Get the full ready-to-send raw request
 	std::string GetRawRequest();
-	
+
 private:
 	// The type of request
 	tRequestType m_RequestType;
 	// The list of headers held by this request
 	std::list<CHttpHeader> m_Headers;
-	
+
 	// The resource that will be requested
 	std::string m_Resource;
-	
+
 	// ### todo: request data
 };
 
