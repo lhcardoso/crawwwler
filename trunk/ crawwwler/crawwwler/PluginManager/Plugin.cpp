d2cf3e5afc4c775a1b847cd1f7c271aa742abc5c
davidjohnarcher <davidjohnarcher@a9951ad4-3145-0410-9025-0dd7c1825583>
2008-02-17 17:43:32 +0000

    Stripped out some functions that will not be used in the design

--- trunk/ crawwwler/crawwwler/PluginManager/Plugin.cpp
+++ trunk/ crawwwler/crawwwler/PluginManager/Plugin.cpp
@@ -11,13 +11,4 @@ CPlugin::~CPlugin() {
 ///////////////////////////////////////////////////////////////
 // Public Methods
 
-bool CPlugin::AddSubscriber(CPlugin *pSubscriber) {
-	m_Subscribers.push_back(pSubscriber);
-	return true;
-}
-
-bool CPlugin::RemoveSubscriber(const CPluginId& Id) {
-	return false;
-}
-
 }
--- trunk/ crawwwler/crawwwler/PluginManager/Plugin.h
+++ trunk/ crawwwler/crawwwler/PluginManager/Plugin.h
@@ -14,31 +14,18 @@ class CPlugin
 public:
 	CPlugin();
 	virtual ~CPlugin();
-	
+
 	// Init the plugin
 	virtual bool Init() = 0;
 	// Start the plugin
 	virtual bool Start() = 0;
 	// Stop the plugin
 	virtual bool Stop() = 0;
-	
-	/////////////////////////////////////////////////////////////////
-	// Public Methods
-	
-	// Add another plugin to the list of subscribers
-	bool AddSubscriber(CPlugin *pSubscriber);
-	// Remove another plugin from the list of subscribers
-	bool RemoveSubscriber(const CPluginId& Id);
-	
-	// As a subscriber, another plugin will push data into this plugin via this function, this will be overridden by inherited classes,
-	// The caller still owns the memory of the data and so it should be copied if needed
-	virtual bool PushData(std::list<void*> Data) = 0;
-	
-private:	
-	
+
+private:
+
 protected:
-	// The other plugins that subscribe to the output of this plugin
-	std::list<CPlugin*> m_Subscribers;
+
 };
 
 }
