cd101be52b2e22a99b876981c3dd408733a25bf7
davidjohnarcher <davidjohnarcher@a9951ad4-3145-0410-9025-0dd7c1825583>
2008-02-17 14:33:53 +0000

    Test functionality now calls the new CDomainCrawler class instead of the deprecated CCrawler. Yay!!

---  crawwwler/crawwwler/src/main.cpp
+++  crawwwler/crawwwler/src/main.cpp
@@ -8,6 +8,7 @@
 #include "../ServerInfo/ServerInfo.h"
 #include "../HttpUtils/Url.h"
 #include "../Utils/LogFile.h"
+#include "../DomainCrawler/DomainCrawler.h"
 
 using namespace Crawwwler;
 
@@ -23,70 +24,22 @@ int main(int argc, char *argv[]) {
 
 	if (ArgCount == 1) {
 
-		std::cout << "Enter a server name" << std::endl;
+		std::cout << "Enter a domain name" << std::endl;
 		return 0;
 	}
 
 	// Get the target server name
-	std::string TargetServerName = argv[1];
+	std::string TargetDomainName = argv[1];
 
-
-	// Create the crawler object
-	CCrawlerPlugin Crawler;
-
-	// Create the url extractor and subscribe it to the crawler plugin's output
-	CUrlExtractor Extractor;
-	Crawler.AddSubscriber(&Extractor);
-	Extractor.AddSubscriber(&Crawler);
-
-	// Add a server
-	CRemoteServer *pServer = new CRemoteServer();
-	// Argument is from the command line
-	if (!pServer->SetServer(TargetServerName)) {
-		std::cout << "Couldn't set server" << std::endl;
-	}
-	CServerInfo *pServerInfo = new CServerInfo();
-	if (!pServerInfo->SetServer(pServer));
-
-	if (!Crawler.AddServer(pServerInfo)) {
-		std::cout << "Couldn't add server" << std::endl;
+	
+	// Crawl the domain
+	CDomainCrawler DomainCrawler;
+	if (!DomainCrawler.Crawl(TargetDomainName)) {
+		std::cout << "Unsuccessful crawl. Boooo!" << std::endl;
 		return 0;
 	}
-
-	// Run a little test which should get all the pages from the site set in the pServer->SetServer() command above
-	bool bRunning = true;
-	while (bRunning) {
-		bRunning = Crawler.Start();
-		// The extractor::Start() currently returns false if there's nothing for it to work on
-		if (!Extractor.Start()) {
-			bRunning = false;
-		}
-
-	}
-
-	// Some dummy output to make us feel better
-
-	std::cout << "Crawl Successful" << std::endl;
-
-	// Get the responses
-	std::list<CHttpResponse*> Responses;
-	Responses = Crawler.CollectResponses();
-
-	std::cout << "Getting Responses" << std::endl;
-	if (Responses.size() == 0) {
-		std::cout << "No Responses" << std::endl;
-		return 0;
-	}
-
-
-	// Output the response
-	CHttpResponse *pResponse = Responses.front();
-	if (!pResponse) {
-		std::cout << "No Response" << std::endl;
-		return 0;
-	}
-
-	std::cout << "Extracted! yay!" << std::endl;
+	// Success
+	std::cout << "Successful Crawl! yay!" << std::endl;
 	CLogFile Logger;
 	Logger.Write("Ok");
 	return 0;
