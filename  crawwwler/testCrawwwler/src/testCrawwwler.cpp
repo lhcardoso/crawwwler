8a6913729592d8926a92626eab5aef51c067c68f
davidjohnarcher <davidjohnarcher@a9951ad4-3145-0410-9025-0dd7c1825583>
2008-02-18 00:40:46 +0000

    Initial import.

---
+++  crawwwler/testCrawwwler/src/testCrawwwler.cpp
@@ -0,0 +1,45 @@
+//============================================================================
+// Name        : testCrawwwler.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "../Utils/LogFile.h"
+#include "../DomainCrawler/DomainCrawler.h"
+using namespace std;
+//using namespace Crawwwler;
+
+int main(int argc, char *argv[]) {
+	// ### Most of this code is for testing only.  As the system matures, we'll work on formalising the running and control of the plugins
+
+		// Parse the command line arguments
+		int ArgCount = 0;
+		while (argv[ArgCount]) {
+			std::cout << argv[ArgCount] << std::endl;
+			ArgCount++;
+		}
+
+		if (ArgCount == 1) {
+
+			std::cout << "Enter a domain name" << std::endl;
+			return 0;
+		}
+
+		// Get the target server name
+		std::string TargetDomainName = argv[1];
+
+		// Crawl the domain
+		Crawwwler::CDomainCrawler DomainCrawler;
+		if (!DomainCrawler.Crawl(TargetDomainName)) {
+			std::cout << "Unsuccessful crawl. Boooo!" << std::endl;
+			return 0;
+		}
+		// Success
+		std::cout << "Successful Crawl! yay!" << std::endl;
+		Crawwwler::CLogFile Logger;
+		Logger.Write("Ok");
+		return 0;
+}
