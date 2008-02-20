8b74371e50e860bb3de7fe2ab05245de754ac005
davidjohnarcher <davidjohnarcher@a9951ad4-3145-0410-9025-0dd7c1825583>
2008-02-20 00:41:45 +0000

    Dependancy files?

---
+++ trunk/ crawwwler/crawwwler/Debug/DomainCrawler/DomainCrawler.d
@@ -0,0 +1,51 @@
+DomainCrawler/DomainCrawler.d DomainCrawler/DomainCrawler.o:  \
+ ../DomainCrawler/DomainCrawler.cpp ../DomainCrawler/DomainCrawler.h \
+  ../DomainCrawler/../Server/RemoteServer.h \
+  ../DomainCrawler/../Server/Server.h \
+  ../DomainCrawler/../Server/../NetworkAPI/Port.h \
+  ../DomainCrawler/../Server/../Url/Url.h \
+  ../DomainCrawler/../RobotsTxt/RobotsTxtFile.h \
+  ../DomainCrawler/../Http/HttpConnector/HttpClient.h \
+  ../DomainCrawler/../Url/ManagedUrlList.h \
+  ../DomainCrawler/../Url/../Url/Url.h \
+  ../DomainCrawler/../Http/HttpUtils/HttpResponseList.h \
+  ../DomainCrawler/../Http/HttpUtils/HttpResponse.h \
+  ../DomainCrawler/../Http/HttpUtils/HttpHeader.h \
+  ../DomainCrawler/../Http/HttpUtils/../../Url/Url.h \
+  ../DomainCrawler/../Http/HttpUtils/HttpResponse.h \
+  ../DomainCrawler/../NetworkAPI/Port.h ../DomainCrawler/../Url/Url.h \
+  ../DomainCrawler/../UrlExtractor/UrlExtractor.h
+
+../DomainCrawler/DomainCrawler.h:
+
+../DomainCrawler/../Server/RemoteServer.h:
+
+../DomainCrawler/../Server/Server.h:
+
+../DomainCrawler/../Server/../NetworkAPI/Port.h:
+
+../DomainCrawler/../Server/../Url/Url.h:
+
+../DomainCrawler/../RobotsTxt/RobotsTxtFile.h:
+
+../DomainCrawler/../Http/HttpConnector/HttpClient.h:
+
+../DomainCrawler/../Url/ManagedUrlList.h:
+
+../DomainCrawler/../Url/../Url/Url.h:
+
+../DomainCrawler/../Http/HttpUtils/HttpResponseList.h:
+
+../DomainCrawler/../Http/HttpUtils/HttpResponse.h:
+
+../DomainCrawler/../Http/HttpUtils/HttpHeader.h:
+
+../DomainCrawler/../Http/HttpUtils/../../Url/Url.h:
+
+../DomainCrawler/../Http/HttpUtils/HttpResponse.h:
+
+../DomainCrawler/../NetworkAPI/Port.h:
+
+../DomainCrawler/../Url/Url.h:
+
+../DomainCrawler/../UrlExtractor/UrlExtractor.h:
---
+++ trunk/ crawwwler/crawwwler/Debug/DomainCrawler/DomainCrawlerList.d
@@ -0,0 +1,43 @@
+DomainCrawler/DomainCrawlerList.d DomainCrawler/DomainCrawlerList.o:  \
+ ../DomainCrawler/DomainCrawlerList.cpp \
+  ../DomainCrawler/DomainCrawlerList.h ../DomainCrawler/DomainCrawler.h \
+  ../DomainCrawler/../Server/RemoteServer.h \
+  ../DomainCrawler/../Server/Server.h \
+  ../DomainCrawler/../Server/../NetworkAPI/Port.h \
+  ../DomainCrawler/../Server/../Url/Url.h \
+  ../DomainCrawler/../RobotsTxt/RobotsTxtFile.h \
+  ../DomainCrawler/../Http/HttpConnector/HttpClient.h \
+  ../DomainCrawler/../Url/ManagedUrlList.h \
+  ../DomainCrawler/../Url/../Url/Url.h \
+  ../DomainCrawler/../Http/HttpUtils/HttpResponseList.h \
+  ../DomainCrawler/../Http/HttpUtils/HttpResponse.h \
+  ../DomainCrawler/../Http/HttpUtils/HttpHeader.h \
+  ../DomainCrawler/../Http/HttpUtils/../../Url/Url.h
+
+../DomainCrawler/DomainCrawlerList.h:
+
+../DomainCrawler/DomainCrawler.h:
+
+../DomainCrawler/../Server/RemoteServer.h:
+
+../DomainCrawler/../Server/Server.h:
+
+../DomainCrawler/../Server/../NetworkAPI/Port.h:
+
+../DomainCrawler/../Server/../Url/Url.h:
+
+../DomainCrawler/../RobotsTxt/RobotsTxtFile.h:
+
+../DomainCrawler/../Http/HttpConnector/HttpClient.h:
+
+../DomainCrawler/../Url/ManagedUrlList.h:
+
+../DomainCrawler/../Url/../Url/Url.h:
+
+../DomainCrawler/../Http/HttpUtils/HttpResponseList.h:
+
+../DomainCrawler/../Http/HttpUtils/HttpResponse.h:
+
+../DomainCrawler/../Http/HttpUtils/HttpHeader.h:
+
+../DomainCrawler/../Http/HttpUtils/../../Url/Url.h:
