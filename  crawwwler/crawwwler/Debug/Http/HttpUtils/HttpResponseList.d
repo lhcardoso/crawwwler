95973f49ec00503e67eddd7625681578f84c2070
davidjohnarcher <davidjohnarcher@a9951ad4-3145-0410-9025-0dd7c1825583>
2008-02-20 00:41:45 +0000

    Dependancy files?

---
+++  crawwwler/crawwwler/Debug/Http/HttpUtils/HttpResponseList.d
@@ -0,0 +1,12 @@
+Http/HttpUtils/HttpResponseList.d Http/HttpUtils/HttpResponseList.o:  \
+ ../Http/HttpUtils/HttpResponseList.cpp \
+  ../Http/HttpUtils/HttpResponseList.h ../Http/HttpUtils/HttpResponse.h \
+  ../Http/HttpUtils/HttpHeader.h ../Http/HttpUtils/../../Url/Url.h
+
+../Http/HttpUtils/HttpResponseList.h:
+
+../Http/HttpUtils/HttpResponse.h:
+
+../Http/HttpUtils/HttpHeader.h:
+
+../Http/HttpUtils/../../Url/Url.h:
---
+++  crawwwler/crawwwler/Debug/UrlExtractor/UrlExtractor.d
@@ -0,0 +1,21 @@
+UrlExtractor/UrlExtractor.d UrlExtractor/UrlExtractor.o:  \
+ ../UrlExtractor/UrlExtractor.cpp ../UrlExtractor/UrlExtractor.h \
+  ../UrlExtractor/../Http/HttpUtils/HttpResponse.h \
+  ../UrlExtractor/../Http/HttpUtils/HttpHeader.h \
+  ../UrlExtractor/../Http/HttpUtils/../../Url/Url.h \
+  ../UrlExtractor/../Url/Url.h ../UrlExtractor/../Url/ManagedUrlList.h \
+  ../UrlExtractor/../Url/../Url/Url.h
+
+../UrlExtractor/UrlExtractor.h:
+
+../UrlExtractor/../Http/HttpUtils/HttpResponse.h:
+
+../UrlExtractor/../Http/HttpUtils/HttpHeader.h:
+
+../UrlExtractor/../Http/HttpUtils/../../Url/Url.h:
+
+../UrlExtractor/../Url/Url.h:
+
+../UrlExtractor/../Url/ManagedUrlList.h:
+
+../UrlExtractor/../Url/../Url/Url.h:
